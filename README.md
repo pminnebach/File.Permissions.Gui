69abf8e5531eb887fef84c4d9f25a956c9192fc4
Philip Minnebach <philip@minnebach.be>
2020-10-28 07:30:25 +0100

    add remaining stuff.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Intune.HV.Tools.Gui
+
+## Requirements
+
+- PowerShell 5.1, or higher.
+
+## How to use
+
+Clone the whole project, including the submodules:
+
+```
+git clone https://github.com/pminnebach/Intune.HV.Tools.Gui --recursive
+```
+
+Run the `build.ps1` script.
+
+Run the `run.ps1` script.
+
+### Preview
+
+![Preview01](Assets/Preview01.jpg)
+
+![Preview02](Assets/Preview02.jpg)
+
+## Shoulders, giants!
+
+- [FoxDeploy XAML to GUI](https://github.com/1RedOne/PowerShell_XAML/)
+- [Idera: Show or hide windows](https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/show-or-hide-windows)
+
+## TODO
+
+- [ ] Add refresh option
---
+++ code.ps1
@@ -0,0 +1,71 @@
+<#---------------
+ | Functions
+ #---------------#>
+
+
+
+
+
+<#---------------
+ | Xaml Objects
+ #---------------#>
+
+#region TabItem: Environment
+
+# $WPFComputernameLabel.Content = $env:COMPUTERNAME
+# $WPFPSVersionLabel.Content = $PSVersionTable.PSVersion.ToString()
+# if ($PSVersionTable.PSVersion.Major -eq 5) {
+#     $WPFOSLabel.Content = "Windows"
+# }
+# else {
+#     $WPFOSLabel.Content = $PSVersionTable.OS
+# }
+
+# Get-Module -Name Hyper-ConvertImage, Microsoft.Graph.Intune, WindowsAutoPilotIntune, Intune.HV.Tools -ListAvailable | Sort-Object Name | ForEach-Object {
+#     $WPFmoduleGrid.AddChild([pscustomobject]@{
+#             ModuleType = $_.ModuleType
+#             Version    = $_.Version
+#             Name       = $_.Name
+#         })
+# }
+
+#endregion
+
+#region TabItem: hvconfig.json
+
+# $HVToolsConfig = Get-HVToolsConfig
+# $WPFhvConfigPathLabel.Content = $HVToolsConfig.hvConfigPath
+# $WPFvmPathLabel.Content = $HVToolsConfig.vmPath
+# $WPFvSwitchNameLabel.Content = $HVToolsConfig.vSwitchName
+# $WPFvLanIdLabel.Content = $HVToolsConfig.vLanId
+
+# $HVToolsConfig.tenantConfig | ForEach-Object {
+#     $WPFTenantGrid.AddChild([PSCustomObject]@{
+#             TenantName = $_.TenantName
+#             ImageName  = $_.ImageName
+#             AdminUpn   = $_.AdminUpn
+#         })
+# }
+
+# $HVToolsConfig.images | ForEach-Object {
+#     $WPFImagesGrid.AddChild([PSCustomObject]@{
+#             imageName    = $_.imageName
+#             imagePath    = $_.imagePath
+#             refImagePath = $_.refImagePath
+#         })
+# }
+
+#endregion
+
+#Reference
+
+#Adding items to a dropdown/combo box
+#$vmpicklistView.items.Add([pscustomobject]@{'VMName'=($_).Name;Status=$_.Status;Other="Yes"})
+
+#Setting the text of a text box to the current PC name
+#$WPFtextBox.Text = $env:COMPUTERNAME
+
+#Adding code to a button, so that when clicked, it pings a system
+# $WPFbutton.Add_Click({ Test-connection -count 1 -ComputerName $WPFtextBox.Text
+# })
+
